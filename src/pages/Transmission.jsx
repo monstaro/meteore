f35bc082fb774eddc5b7cfa2748253c64b70d999
Cody <codysmith2814@gmail.com>
2026-02-10 15:36:18 -0700

    updt

--- src/pages/Transmission.jsx
+++ src/pages/Transmission.jsx
@@ -7,27 +7,26 @@ export default function Transmission() {
       {/* <div /> */}
       <div className="flicker-wrapper transmission-text">
         <p>
-          <b>transmission 001 — feb 9 2029</b>
+          <b>transmission 001 — feb 9 2026</b>
         </p>
         <p>something was received here. we are still decoding.</p>
         <br></br>
         <p>
-          <b>transmission 002 — feb 10 2029</b>
+          <b>transmission 002 — feb 10 2026</b>
         </p>
         <p>
-          yesterday, a periodic, non-terrestrial 11-minute signal was
+          yesterday, a non-repeating, non-terrestrial 11-minute signal was
           intercepted by the ALMA observatory. the source is currently
-          positioned at roughly 0.2 light years (12648.17 AU). <br></br>
-          while the source remains optically invisible, the radio signature is
-          unmistakable. <br></br>
-          <br></br>Carrier Frequency: 8.41 GHz (X-band) <br></br>Duration: 11m
-          04s non-repeating <br></br>
-          Doppler Profile: Significant blueshift (−210 km/s relative to the Sun)
-          Direct, non-Keplerian intercept course with inner solar system
+          positioned at roughly 22.40 AU. <br></br> <br></br>
+          spectral return indicates a high-density metallic core, suggesting a
+          massive anomalous body. the doppler profile indicates a significant
+          blueshift (−210 km/s relative to the Sun) suggesting a direct
+          intercept course with inner solar system. while the object has no
+          visual confirmation, the radio signature is certain.
           <br></br>
-          <br></br>the object's velocity and trajectory indicate a deep-impact
-          event timeframe of less than a decade. investigation has been moved to
-          a discrete, compartmentalized channel.
+          <br></br>the object's velocity and trajectory indicate a potential
+          deep-impact event timeframe of less than a 365 days. further
+          communications will remain on this secure NASA intranet channel.
         </p>
       </div>
       <FracturedMeteore />
